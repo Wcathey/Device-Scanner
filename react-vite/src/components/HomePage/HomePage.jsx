b67fb8a1a16c8495c61d799b7bdde295731554e2
William <wcathey1604@gmail.com>
2024-12-10 20:20:23 -0600

    added react components for homepage, started front end

---
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdDevicesOther } from "react-icons/md";
+import { FiUploadCloud } from "react-icons/fi";
+
+import "./HomePage.css";
+
+function HomePage() {
+    return (
+        <div className="hp-container">
+            <div className="hp-side-bar">
+                <ul className="hp-sb-list">
+                    <li>
+                        <NavLink to="/transfers">
+                            <FiUploadCloud className="icon" />
+                            Uploads
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/devices">
+                            <MdDevicesOther className="icon" />
+                            Devices
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/settings">
+                            <IoSettingsOutline className="icon" />
+                            Settings
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,15 +4,33 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+    <div className="nav-container">
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+      <div className="nav-bar">
+        <ul className="nav-list">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+
+          <li>
+            <ProfileButton />
+          </li>
+        </ul>
+      </div>
+
+      <div id="nav-title">
+        <h1>Scanner Archive</h1>
+      </div>
+      <div className="nav-search-bar">
+
+        <label>Search:</label>
+        <input
+          placeholder="Search for an image..."
+        >
+        </input>
+      </div>
+    </div>
+
   );
 }
 
